796147e98a2339f092d647b7b4cdf1eac2921928
Karunyareddy49 <pitchapatikarunyareddy@gmail.com>
2026-03-23 20:54:11 +0530

    Change app.run to use environment PORT

--- app.py
+++ app.py
@@ -258,4 +258,5 @@ def server_error(e):
 # Run
 # --------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render provides PORT
+    app.run(host="0.0.0.0", port=port)
